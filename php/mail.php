0fad7b9ce0eb63870bb715d64d693b0a602edf60
Bryan Kristofferson <bryangators510@gmail.com>
2017-03-04 23:09:26 -0500

    test filters

--- php/mail.php
+++ php/mail.php
@@ -16,11 +16,17 @@ if (isset($_POST['submit'])) {
 	$message2 = $name . " your email was sent. We will get back to you as soon as possible. Thank you and God Bless." .
 				"\n\n". "Original Message: " . "\n" .   $_POST['message'];
 
-	$headers = "From: " . $from . "\r\n";
-	$headers2 = "From: " . $to . "\r\n";
+	// $headers .= "Reply-To: The Sender <". $to . ">\r\n"; 
+ //  	$headers .= "Return-Path: The Sender <". $to . ">\r\n";
+ //  	$headers .= "From: The Sender <". $to . ">\r\n";
+ //  	$headers .= "MIME-Version: 1.0\r\n";
+ //    $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
+ //    $headers .= "X-Priority: 3\r\n";
+ //    $headers .= "X-Mailer: PHP". phpversion() ."\r\n"  
+	$headers = "From: bryan@bryankristofferson.com";
 
 	mail($to, $subject, $message, $headers);
-	mail($from, $subject2, $message2, $headers2);
+	// mail($from, $subject2, $message2, $headers2);
 
 	$msg = "Thank You! We will be in contact shortly.";
 
