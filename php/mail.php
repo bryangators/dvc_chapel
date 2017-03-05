611c85a81b2dc0f6f7bc5833ec0350bd249617f0
Bryan Kristofferson <bryangators510@gmail.com>
2017-03-04 22:55:48 -0500

    Attempting to fix spam issues for php mail

--- php/mail.php
+++ php/mail.php
@@ -16,8 +16,8 @@ if (isset($_POST['submit'])) {
 	$message2 = $name . " your email was sent. We will get back to you as soon as possible. Thank you and God Bless." .
 				"\n\n". "Original Message: " . "\n" .   $_POST['message'];
 
-	$headers = "From: " . $from;
-	$headers2 = "From: " . $to;
+	$headers = "From: " . $from . "\r\n";
+	$headers2 = "From: " . $to . "\r\n";
 
 	mail($to, $subject, $message, $headers);
 	mail($from, $subject2, $message2, $headers2);
