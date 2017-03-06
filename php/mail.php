aa46d7e7934665a9088ba2cdca3c52da4ef4f4a5
Bryan Kristofferson <bryangators510@gmail.com>
2017-03-06 10:40:20 -0500

    test php mail

--- php/mail.php
+++ php/mail.php
@@ -16,11 +16,14 @@ if (isset($_POST['submit'])) {
 	$message2 = $name . " your email was sent. We will get back to you as soon as possible. Thank you and God Bless." .
 				"\n\n". "Original Message: " . "\n" .   $_POST['message'];
 
-	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: " . $from . "\r\n";
-	$headers .= "Return-Path: " . $from . "\r\n";
-	$headers .= "CC: " . $to . "\r\n";
-	$headers .= "BCC: " . $to . "\r\n";
+	// $headers = "From: " . $from . "\r\n";
+	// $headers .= "Reply-To: " . $from . "\r\n";
+	// $headers .= "Return-Path: " . $from . "\r\n";
+	// $headers .= "CC: " . $to . "\r\n";
+	// $headers .= "BCC: " . $to . "\r\n";
+	$headers = 'From: -f'. $from . " " .
+	'Reply-To: '. $from . " " .
+	'X-Mailer: PHP/' . phpversion();
 
 	mail($to, $subject, $message, $headers);
 	// mail($from, $subject2, $message2, $headers2);
