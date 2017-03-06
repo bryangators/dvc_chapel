8027ad727d38bb9e66fb0ab2aeabe7e8f9fada9d
Bryan Kristofferson <bryangators510@gmail.com>
2017-03-06 11:10:40 -0500

    testing php mail

--- php/mail.php
+++ php/mail.php
@@ -3,23 +3,23 @@
 if (isset($_POST['submit'])) {
 	//email to recipient of message
 	$to = "bryangators510@gmail.com";
-
+	$host = "bryan@bryankristofferson.com";
 	$from = $_POST['email'];
 	$name = $_POST['name'];
 	$phone = $_POST['phone'];
 
-	$subject = "Contact Form Message";
+	$subject = "Contact Form Message - Deltona Victory Chapel";
 	
 	$message = "Message from: " . $name . "\n" . "Phone: " . $phone . "\n\n" .
 			   $_POST['message'];
 	// $message2 = $name . " your email was sent. We will get back to you as soon as possible. Thank you and God Bless." .
 	// 			"\n\n". "Original Message: " . "\n" .   $_POST['message'];
 
-	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: " . $from . "\r\n";
-	$headers .= "Return-Path: " . $from . "\r\n";
+	$headers = "From: " . $host . "\r\n";
+	$headers .= "Reply-To: " . $host . "\r\n";
+	$headers .= "Return-Path: " . $host . "\r\n";
 		
-	mail('bryangators510@gmail.com', 'TEST', 'TEST', null, '-fbryan@bryankristofferson.com'); 
+	mail($host, $subject, $message, $headers, '-fbryan@bryankristofferson.com'); 
 
 	// mail($to, $subject, $message, $headers, "-fbryan@bryankristofferson.com");
 	// mail($from, $subject, $message, $headers);
