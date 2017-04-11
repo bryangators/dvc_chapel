15e7849c91519dee342a265c74dbffd9c7d5d835
Bryan Kristofferson <bryangators510@gmail.com>
2017-04-10 22:40:03 -0400

    fixed favicon issues

--- index.php
+++ index.php
@@ -8,6 +8,16 @@ include('php/site_version.php')
 <!DOCTYPE html>
 <html>
 <head>
+
+<link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" href="images/favicon/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="images/favicon/favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="images/favicon/manifest.json">
+<link rel="mask-icon" href="images/favicon/safari-pinned-tab.svg" color="#5bbad5">
+<link rel="shortcut icon" href="images/favicon/favicon.ico">
+<meta name="msapplication-config" content="images/favicon/browserconfig.xml">
+<meta name="theme-color" content="#ffffff">
+
 <!-- style sheet -->
 <link rel="stylesheet" type="text/css" href="css/indexStyles.css?v=<?php echo $version; ?>">
 <!-- java script files -->
